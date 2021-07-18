2e9688c0956a124475ac335c4c3393b8bc21f310
Felipe Chagas <fechagas.dev@gmail.com>
2021-07-17 23:43:03 -0300

    53

--- __tests__/PasswordReset.spec.js
+++ __tests__/PasswordReset.spec.js
@@ -53,11 +53,19 @@ const addUser = async (user = { ...activeUser }) => {
 };
 
 const postPasswordReset = (email = 'user1@mail.com', options = {}) => {
-  const agent = request(app).post('/api/1.0/users/password');
+  const agent = request(app).post('/api/1.0/user/password');
   if (options.language) {
     agent.set('Accept-Language', options.language);
   }
   return agent.send({ email: email });
+};
+
+const putPasswordUpdate = (body = {}, options = {}) => {
+  const agent = request(app).put('/api/1.0/user/password');
+  if (options.language) {
+    agent.set('Accept-Language', options.language);
+  }
+  return agent.send(body);
 };
 
 describe('Password Reset Request', () => {
@@ -71,11 +79,11 @@ describe('Password Reset Request', () => {
     ${'br'}  | ${br.email_not_inuse}
     ${'en'}  | ${en.email_not_inuse}
   `(
-    'returns errors body with $message for unknown e-mail for password request when language is $language',
+    'returns error body with $message for unknown e-mail for password request when language is $language',
     async ({ language, message }) => {
       const nowInMillis = new Date().getTime();
       const response = await postPasswordReset('user1@mail.com', { language: language });
-      expect(response.body.path).toBe('/api/1.0/users/password');
+      expect(response.body.path).toBe('/api/1.0/user/password');
       expect(response.body.timestamp).toBeGreaterThan(nowInMillis);
       expect(response.body.message).toBe(message);
     }
@@ -146,4 +154,68 @@ describe('Password Reset Request', () => {
     const response = await postPasswordReset(user.email, { language });
     expect(response.body.message).toBe(message);
   });
+});
+
+describe('Password Update', () => {
+  it('returns 403 when password update request does not have the valid password reset token', async () => {
+    const response = await putPasswordUpdate({ password: 'P4ssword', passwordResetToken: 'abcd' });
+    expect(response.status).toBe(403);
+  });
+
+  it.each`
+    language | message
+    ${'br'}  | ${br.unauthorized_password_reset}
+    ${'en'}  | ${en.unauthorized_password_reset}
+  `(
+    'returns error body with $message when language is set to $language after trying to update with invalid token',
+    async ({ language, message }) => {
+      const nowInMillis = new Date().getTime();
+      const response = await putPasswordUpdate({ password: 'P4ssword', passwordResetToken: 'abcd' }, { language });
+      expect(response.body.path).toBe('/api/1.0/user/password');
+      expect(response.body.timestamp).toBeGreaterThan(nowInMillis);
+      expect(response.body.message).toBe(message);
+    }
+  );
+
+  it('returns 403 when password update request with invalid password pattern and reset token is invalid', async () => {
+    const response = await putPasswordUpdate({ password: 'not-valid', passwordResetToken: 'abcd' });
+    expect(response.status).toBe(403);
+  });
+
+  it('returns 400 bad request when trying to update with invalid password and the reset token is valid', async () => {
+    const user = await addUser();
+    user.passwordResetToken = 'test-token';
+    await user.save();
+    const response = await putPasswordUpdate({ password: 'not-valid', passwordResetToken: 'test-token' });
+    expect(response.status).toBe(400);
+  });
+
+  it.each`
+    language | value              | message
+    ${'en'}  | ${null}            | ${en.password_null}
+    ${'en'}  | ${'P4ssw'}         | ${en.password_size}
+    ${'en'}  | ${'alllowercase'}  | ${en.password_pattern}
+    ${'en'}  | ${'ALLUPERCASE'}   | ${en.password_pattern}
+    ${'en'}  | ${'1234567890'}    | ${en.password_pattern}
+    ${'en'}  | ${'lowerandUPPER'} | ${en.password_pattern}
+    ${'en'}  | ${'lowerand123'}   | ${en.password_pattern}
+    ${'en'}  | ${'UPPERAND123'}   | ${en.password_pattern}
+    ${'br'}  | ${null}            | ${br.password_null}
+    ${'br'}  | ${'P4ssw'}         | ${br.password_size}
+    ${'br'}  | ${'alllowercase'}  | ${br.password_pattern}
+    ${'br'}  | ${'ALLUPERCASE'}   | ${br.password_pattern}
+    ${'br'}  | ${'1234567890'}    | ${br.password_pattern}
+    ${'br'}  | ${'lowerandUPPER'} | ${br.password_pattern}
+    ${'br'}  | ${'lowerand123'}   | ${br.password_pattern}
+    ${'br'}  | ${'UPPERAND123'}   | ${br.password_pattern}
+  `(
+    'returns password validation error $message when language is set to $language and the value is this $value',
+    async ({ language, message, value }) => {
+      const user = await addUser();
+      user.passwordResetToken = 'test-token';
+      await user.save();
+      const response = await putPasswordUpdate({ password: value, passwordResetToken: 'test-token' }, { language });
+      expect(response.body.validationErrors.password).toBe(message);
+    }
+  );
 });
--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -5,6 +5,7 @@ const { check, validationResult } = require('express-validator');
 const ValidationException = require('../error/ValidationException');
 const pagination = require('../middleware/pagination');
 const ForbiddenException = require('../error/ForbiddenException');
+const User = require('./User');
 
 router.post(
   '/api/1.0/users',
@@ -97,19 +98,43 @@ router.delete('/api/1.0/users/:id', async (req, res, next) => {
   res.send();
 });
 
-router.post(
-  '/api/1.0/users/password',
-  check('email').isEmail().withMessage('email_invalid'),
+router.post('/api/1.0/user/password', check('email').isEmail().withMessage('email_invalid'), async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(new ValidationException(errors.array()));
+  }
+  try {
+    await UserService.passwordResetRequest(req.body.email);
+    return res.send({ message: req.t('password_reset_request_success') });
+  } catch (err) {
+    next(err);
+  }
+});
+
+const passwordResetTokenValidator = async (req, res, next) => {
+  const user = await User.findOne({ where: { passwordResetToken: req.body.passwordResetToken } });
+  if (!user) {
+    return next(new ForbiddenException('unauthorized_password_reset'));
+  }
+  next();
+};
+
+router.put(
+  '/api/1.0/user/password',
+  passwordResetTokenValidator,
+  check('password')
+    .notEmpty()
+    .withMessage('password_null')
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage('password_size')
+    .bail()
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
+    .withMessage('password_pattern'),
   async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return next(new ValidationException(errors.array()));
-    }
-    try {
-      await UserService.passwordResetRequest(req.body.email);
-      return res.send({ message: req.t('password_reset_request_success') });
-    } catch (err) {
-      next(err);
     }
   }
 );
